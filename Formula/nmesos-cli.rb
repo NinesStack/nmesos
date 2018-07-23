00bfdd09a3a34ca48c6bc7af0ab33b8f21e259b8
Mihai Todor <todormihai@gmail.com>
2018-07-23 16:36:35 +0100

    Bump up the version again
    The previous one didn't have the fix :(

--- Formula/nmesos-cli.rb
+++ Formula/nmesos-cli.rb
@@ -3,8 +3,8 @@ require 'formula'
 class NmesosCli < Formula
   desc "Nmesos is a CLI tool to deploy into Mesos."
   homepage "https://github.com/Nitro/nmesos"
-  url "https://s3-us-west-2.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.6/nmesos-cli-0.2.6.tgz"
-  sha256 "381012157e462455a4461014e8ed3603966ae0aa36da4a0c64d603442c576bd2"
+  url "https://s3-us-west-2.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.7/nmesos-cli-0.2.7.tgz"
+  sha256 "c456e01c279bd4d7bd68781411267811da637ad9e7d5c99c2f33c57a30c49582"
 
   def install
     bin.install 'nmesos'
