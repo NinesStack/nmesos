06bcd09e27eb90fda60be236db733a278ee52231
Sofian Brabez <sofian.brabez@gonitro.com>
2018-10-16 18:29:26 +0100

    Add Formula --with-bash-completion option to install conditionally

--- Formula/nmesos-cli.rb
+++ Formula/nmesos-cli.rb
@@ -6,8 +6,15 @@ class NmesosCli < Formula
   url "https://s3-us-west-2.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.7/nmesos-cli-0.2.7.tgz"
   sha256 "c456e01c279bd4d7bd68781411267811da637ad9e7d5c99c2f33c57a30c49582"
 
+  option "with-bash-completion", "Install bash-completion"
+
+  depends_on "bash-completion" => :optional
+
   def install
+    if build.with? "bash-completion"
+      bash_completion.install "./contrib/etc/bash_completion.d/nmesos"
+    end
+
     bin.install 'nmesos'
-    bash_completion.install 'contrib/etc/bash_completion.d/nmesos'
   end
 end
