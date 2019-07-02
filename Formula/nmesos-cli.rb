d72ea3835004e88255fd7d2236c146814516becb
Felix <felix.g.borrego@gmail.com>
2019-07-02 12:11:33 +0100

    Bump version to 0.2.13 including environment diff warning

--- Formula/nmesos-cli.rb
+++ Formula/nmesos-cli.rb
@@ -3,8 +3,8 @@ require 'formula'
 class NmesosCli < Formula
   desc "Nmesos is a CLI tool to deploy into Mesos."
   homepage "https://github.com/Nitro/nmesos"
-  url "https://s3-us-west-2.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.12/nmesos-cli-0.2.12.tgz"
-  sha256 "ed6081eeb458e5a91218793c216bc81f545f9793a0528dc05b493cace95348c5"
+  url "https://s3-us-west-2.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.13/nmesos-cli-0.2.13.tgz"
+  sha256 "b6db8d94ce4d58d5f12b856761d256b1fbe99ae51a7bc9a802b158ce6fdce562"
 
   option "with-bash-completion", "Install bash-completion"
 
