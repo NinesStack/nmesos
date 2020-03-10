75955cbad0af570d284864c4c3fbf99a3d5d8678
Tom Patterer <t.patterer@gmail.com>
2020-03-09 17:33:13 -0700

    Remove 'curl' from url

--- Formula/nmesos-cli.rb
+++ Formula/nmesos-cli.rb
@@ -3,7 +3,7 @@ require 'formula'
 class NmesosCli < Formula
   desc "Nmesos is a CLI tool to deploy into Mesos."
   homepage "https://github.com/Nitro/nmesos"
-  url "curl https://nmesos-releases.s3-eu-west-1.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.19/nmesos-cli-0.2.19.tgz"
+  url "https://nmesos-releases.s3-eu-west-1.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.19/nmesos-cli-0.2.19.tgz"
   sha256 "6d2403cd27099ee2baf0578a6ae5e03c267a77c109d5ae1a8cfab479c8a5c7d4"
 
   option "with-bash-completion", "Install bash-completion"
