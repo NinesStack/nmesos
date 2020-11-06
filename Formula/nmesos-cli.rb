c83cd5922a9066ceb3d6516112f37937d888828e
Roland Tritsch <roland@tritsch.org>
2020-11-06 12:34:34 +0000

    Updating the sha (again).

--- Formula/nmesos-cli.rb
+++ Formula/nmesos-cli.rb
@@ -4,7 +4,7 @@ class NmesosCli < Formula
   desc "Nmesos is a CLI tool to deploy into Mesos."
   homepage "https://github.com/Nitro/nmesos"
   url "https://nmesos-releases.s3-eu-west-1.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.21/nmesos-cli-0.2.21.tgz"
-  sha256 "9b406f4147c4061f6bc66e497b187fdb3f3078a6bcc966b73c6ce5505fb1a968"
+  sha256 "fe9b077aebeaa4d58e21adcb581543086f874f2d97752f20807673ab259d4357"
 
   option "with-bash-completion", "Install bash-completion"
 
