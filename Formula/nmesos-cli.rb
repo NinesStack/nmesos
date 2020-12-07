58e8ad1ba1b2c319eb6b7dce78aaa048cd0034f7
Roland Tritsch <roland@tritsch.org>
2020-12-07 09:53:04 +0000

    Release 0.2.23.

--- Formula/nmesos-cli.rb
+++ Formula/nmesos-cli.rb
@@ -7,8 +7,8 @@ require 'formula'
 class NmesosCli < Formula
   desc "Nmesos is a CLI tool to deploy into Mesos."
   homepage "https://github.com/Nitro/nmesos"
-  url "https://nmesos-releases.s3-eu-west-1.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.22/nmesos-cli-0.2.22.tgz"
-  sha256 "fccac4920f2317154caa4c099756dcd183d561d42ba782be9c190dfb52bc2e53"
+  url "https://nmesos-releases.s3-eu-west-1.amazonaws.com/nitro-public/repo/nitro/nmesos-cli/0.2.23/nmesos-cli-0.2.23.tgz"
+  sha256 "c1e7d3f467e47de7c24107e3f2464c773a6a9528d27c6b58776fa1d4a198b568"
 
   option "with-bash-completion", "Install bash-completion"
 
