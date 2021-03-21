d6e7f2cead828ed5fce9b6a7a403514c5fdeb07b
Roland Tritsch <roland@tritsch.org>
2021-03-21 09:01:34 +0000

    Fixed/upgraded upickle. Code compiles.

--- src/main/scala/com/nitro/nmesos/singularity/model.scala
+++ src/main/scala/com/nitro/nmesos/singularity/model.scala
@@ -3,6 +3,7 @@ package com.nitro.nmesos.singularity
 import com.nitro.nmesos.singularity.ModelConversions.DeployId
 import com.nitro.nmesos.singularity.model.SingularityRequestParent.SingularityActiveDeployResponse
 import com.nitro.nmesos.singularity.model.SingularityTask.SingularityTaskInfo
+import com.nitro.nmesos.util.CustomPicklers.OptionPickler.{ReadWriter => RW, macroRW}
 
 /**
   * Subset of the Singularity api Model.
@@ -19,6 +20,10 @@ object model {
       diskMb: Int
   )
 
+  object SingularityResources {
+    implicit val rw: RW[SingularityResources] = macroRW
+  }
+
   case class SingularityDockerPortMapping(
       containerPort: Int,
       hostPortType: String,
@@ -26,6 +31,10 @@ object model {
       hostPort: Int,
       protocol: String
   )
+
+  object SingularityDockerPortMapping {
+    implicit val rw:RW[SingularityDockerPortMapping] = macroRW
+  }
 
   case class SingularityDockerInfo(
       network: String,
@@ -35,7 +44,15 @@ object model {
       dockerParameters: Seq[SingularityDockerParameter]
   )
 
+  object SingularityDockerInfo {
+    implicit val rw:RW[SingularityDockerInfo] = macroRW
+  }
+
   case class SingularityDockerParameter(key: String, value: String)
+
+  object SingularityDockerParameter {
+    implicit val rw:RW[SingularityDockerParameter] = macroRW
+  }
 
   case class SingularityVolume(
       hostPath: String,
@@ -43,11 +60,19 @@ object model {
       mode: String
   )
 
+  object SingularityVolume {
+    implicit val rw:RW[SingularityVolume] = macroRW
+  }
+
   case class SingularityContainerInfo(
       `type`: String,
       docker: SingularityDockerInfo,
       volumes: Seq[SingularityVolume]
   )
+
+  object SingularityContainerInfo {
+    implicit val rw:RW[SingularityContainerInfo] = macroRW
+  }
 
   case class SingularityDeploy(
       requestId: String,
@@ -69,6 +94,10 @@ object model {
       healthcheckMaxTotalTimeoutSeconds: Option[Int] = None
   )
 
+  object SingularityDeploy {
+    implicit val rw:RW[SingularityDeploy] = macroRW
+  }
+
   case class SingularityRequest(
       id: String,
       requestType: String,
@@ -80,11 +109,19 @@ object model {
       requiredRole: Option[String] = None
   )
 
+  object SingularityRequest {
+    implicit val rw:RW[SingularityRequest] = macroRW
+  }
+
   case class SingularityDeployRequest(
       deploy: SingularityDeploy,
       message: String,
       unpauseOnSuccessfulDeploy: Boolean
   )
+
+  object SingularityDeployRequest {
+    implicit val rw:RW[SingularityDeployRequest] = macroRW
+  }
 
   case class SingularityScaleRequest(
       message: String,
@@ -93,11 +130,21 @@ object model {
         true //If set to true, healthchecks will be skipped while scaling this request (only)
   )
 
+  object SingularityScaleRequest {
+    implicit val rw:RW[SingularityScaleRequest] = macroRW
+  }
+
   object SingularityRequestParent {
     case class SingularityActiveDeployResponse(
         id: DeployId,
         resources: SingularityResources
     )
+
+    object SingularityActiveDeployResponse {
+      implicit val rw:RW[SingularityActiveDeployResponse] = macroRW
+    }
+
+    implicit val rw:RW[SingularityRequestParent] = macroRW
   }
 
   case class SingularityRequestParent(
@@ -113,18 +160,34 @@ object model {
       deployFailures: Seq[SingularityDeployFailure] = Seq.empty
   )
 
+  object SingularityDeployResult {
+    implicit val rw:RW[SingularityDeployResult] = macroRW
+  }
+
   case class SingularityDeployFailure(
       reason: String,
       taskId: SingularityTaskId,
       message: Option[String] = None
   )
 
+  object SingularityDeployFailure {
+    implicit val rw:RW[SingularityDeployFailure] = macroRW
+  }
+
   case class SingularityUpdateResult(state: String)
+
+  object SingularityUpdateResult {
+    implicit val rw:RW[SingularityUpdateResult] = macroRW
+  }
 
   case class SingularityDeployHistory(
       deploy: SingularityDeploy,
       deployResult: Option[SingularityDeployResult] = None
   )
+
+  object SingularityDeployHistory {
+    implicit val rw:RW[SingularityDeployHistory] = macroRW
+  }
 
   case class SingularityPendingDeploy(
       currentDeployState: String,
@@ -132,9 +195,21 @@ object model {
       deployProgress: SingularityDeployProgress
   )
 
+  object SingularityPendingDeploy {
+    implicit val rw:RW[SingularityPendingDeploy] = macroRW
+  }
+
   case class SingularityDeployProgress(targetActiveInstances: Int)
 
+  object SingularityDeployProgress {
+    implicit val rw:RW[SingularityDeployProgress] = macroRW
+  }
+
   case class SingularityDeployMarker(requestId: String, deployId: String)
+
+  object SingularityDeployMarker {
+    implicit val rw:RW[SingularityDeployMarker] = macroRW
+  }
 
   case class SingularityTaskId(
       requestId: String,
@@ -145,10 +220,18 @@ object model {
       instanceNo: Int
   )
 
+  object SingularityTaskId {
+    implicit val rw:RW[SingularityTaskId] = macroRW
+  }
+
   case class SingularityTaskIdHistory(
       lastTaskState: String,
       taskId: SingularityTaskId
   )
+
+  object SingularityTaskIdHistory {
+    implicit val rw:RW[SingularityTaskIdHistory] = macroRW
+  }
 
   object SingularityTask {
 
@@ -157,14 +240,31 @@ object model {
         hostPort: Int
     )
 
+    object SingularityTasKDockerPortMapping {
+      implicit val rw:RW[SingularityTasKDockerPortMapping] = macroRW
+    }
+
     case class SingularityTaskDockerInfo(
         portMappings: Seq[SingularityTasKDockerPortMapping]
     )
 
+    object SingularityTaskDockerInfo {
+      implicit val rw:RW[SingularityTaskDockerInfo] = macroRW
+    }
+
     case class SingularityTaskIContainerInfo(docker: SingularityTaskDockerInfo)
+
+    object SingularityTaskIContainerInfo {
+      implicit val rw:RW[SingularityTaskIContainerInfo] = macroRW
+    }
 
     case class SingularityTaskInfo(container: SingularityTaskIContainerInfo)
 
+    object SingularityTaskInfo {
+      implicit val rw:RW[SingularityTaskInfo] = macroRW
+    }
+
+    implicit val rw:RW[SingularityTask] = macroRW
   }
 
   case class SingularityTask(
@@ -175,7 +275,19 @@ object model {
 
   case class Offer(hostname: String)
 
+  object Offer {
+    implicit val rw:RW[Offer] = macroRW
+  }
+
   case class SingularityLog(data: String, offset: Int)
 
+  object SingularityLog {
+    implicit val rw:RW[SingularityLog] = macroRW
+  }
+
   case class SingularityTaskIdsByStatus(healthy: Seq[SingularityTaskId])
+
+  object SingularityTaskIdsByStatus {
+    implicit val rw:RW[SingularityTaskIdsByStatus] = macroRW
+  }
 }
